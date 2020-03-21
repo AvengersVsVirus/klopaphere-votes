57c2dffafcd9e04d6e6b55119d7d4cea8d2ee8b1
Marco Klaassen <marco.klaassen@lhind.dlh.de>
2020-03-21 23:00:17 +0100

    feat(kafka): add cert

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -1,5 +1,6 @@
 mvn package
 mkdir -p oc_deploy/lib
+cp -r config/kafka.cert oc_deploy
 cp -r target/lib/* oc_deploy/lib
 cp target/klopaphere-votes-1.0-SNAPSHOT-runner.jar oc_deploy/klopaphere-votes.jar
 cp src/main/docker/Dockerfile.ocp oc_deploy/
