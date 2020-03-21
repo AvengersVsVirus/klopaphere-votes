a03c844116df1a6a41e41717af59887fe4a0bc86
Jonas Janz <jonas@janz.digital>
2020-03-21 19:19:17 +0100

    delete integration test

--- src/test/java/de/klopaphere/votes/NativeVoteResourceIT.java
+++
@@ -1,9 +0,0 @@
-package de.klopaphere.votes;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeVoteResourceIT extends VoteResourceTest {
-
-  // Execute the same tests but in native mode.
-}
