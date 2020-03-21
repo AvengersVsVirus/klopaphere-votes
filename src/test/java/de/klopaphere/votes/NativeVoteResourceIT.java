93903b08203d721d1b506911d560fc8514d700da
Marco Klaassen <marco.klaassen@lhind.dlh.de>
2020-03-21 11:06:06 +0100

    I'll explain when you're older! 🦊

---
+++ src/test/java/de/klopaphere/votes/NativeVoteResourceIT.java
@@ -0,0 +1,9 @@
+package de.klopaphere.votes;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeVoteResourceIT extends VoteResourceTest {
+
+    // Execute the same tests but in native mode.
+}
